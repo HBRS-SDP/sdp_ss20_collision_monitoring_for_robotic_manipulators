79407ed27a24b3fdd3241e4de73b78cfc8007412
Brennan <brennan.penfold@smail.inf.h-brs.de>
2020-04-29 15:14:27 +0200

    Copy from previous repo

---
+++ src/model_interpreter.cpp
@@ -0,0 +1,1 @@
+#include "model_interpreter.h"
---
+++ src/model_interpreter.h
@@ -0,0 +1,12 @@
+/* Model Interpreter header file
+This module contains all the files associated with
+importing and handling the models of the robot manipulator
+*/
+
+//header wrappers to avoid include twice conflicts
+#ifndef MODEL_INTERPRETER_H
+#define MODEL_INTERPRETER_H
+
+// put code here
+
+#endif
