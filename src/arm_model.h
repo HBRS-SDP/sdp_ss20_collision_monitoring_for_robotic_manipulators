3475ecbaf1eb4ecce8b9d90f3ff930d40f932201
Brennan <brennan.penfold@smail.inf.h-brs.de>
2020-05-11 12:52:09 +0200

    Initialisation and basic properties

---
+++ src/arm_model.h
@@ -0,0 +1,43 @@
+#ifndef ARM_MODEL_H
+#define ARM_MODEL_H
+
+// KDL Libraries
+#include <kdl/tree.hpp>
+#include <kdl/treefksolver.hpp>
+#include <kdl/treefksolverpos_recursive.hpp>
+#include <kdl_parser/kdl_parser.hpp>
+
+#include <vector>
+
+#include "basic_shapes.h"
+
+
+class KinovaArm
+{
+    private:
+        KDL::Tree arm_tree;
+        int nr_joints;
+        KDL::Frame pos;
+
+    public:
+        //Constructor
+        KinovaArm(std::string urdf_filename);
+        //Destructor
+        ~KinovaArm();
+
+        bool updatePose(std::vector<double>);
+        std::vector<Link> links;
+}
+
+class Link: public shapes::Cylinder
+{
+    private:
+        //any private variables
+    public:
+        Link* parent_link;
+        std::vector<double> base_point;
+        std::vector<double> endpoint;
+
+}
+
+#endif // ARM_MODEL_H
