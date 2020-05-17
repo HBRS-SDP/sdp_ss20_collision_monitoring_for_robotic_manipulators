3a90b4809a72f8208aec5957d60a784557cf44e4
Brennan Penfold <bjp209@hotmail.com>
2020-05-17 11:54:04 +0200

    Added potential test file to use

---
+++ src/test.cpp
@@ -0,0 +1,16 @@
+// Pulled from https://github.com/catchorg/Catch2/blob/master/docs/tutorial.md
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "arm_model.h"
+
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
