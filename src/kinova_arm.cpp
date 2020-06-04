5404375d104aabfb806cc61b69abb33e45751b22
Brennan Penfold <bjp209@hotmail.com>
2020-06-04 11:01:13 +0200

    [arm] kdl_parser compiling with test

--- src/kinova_arm.cpp
+++ src/kinova_arm.cpp
@@ -4,13 +4,13 @@
 KinovaArm::KinovaArm(std::string urdf_filename){
     // Import the tree from urdf
 	// pulled from https://wiki.ros.org/kdl_parser/Tutorials/Start%20using%20the%20KDL%20parser
-	// if (!kdl_parser::treeFromFile(urdf_filename, arm_tree)){
-	// 	std::cout << "Failed to construct kdl tree" << std::endl;
-	// }
-	// arm_tree.getChain("base_link", "EndEffector_Link", chain);
+	if (!kdl_parser::treeFromFile(urdf_filename, arm_tree)){
+		std::cout << "Failed to construct kdl tree" << std::endl;
+	}
+	arm_tree.getChain("base_link", "EndEffector_Link", chain);
 
 
-	// nr_joints = chain.getNrOfJoints();
+	nr_joints = chain.getNrOfJoints();
 }
 
 KinovaArm::~KinovaArm(){
