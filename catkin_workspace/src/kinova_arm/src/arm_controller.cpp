4a30eeb4d282664a59adeba7de2e87fdd986bff0
Brennan Penfold <bjp209@hotmail.com>
2020-06-10 17:54:20 +0200

    [ROS] update ready for merge

--- catkin_workspace/src/kinova_arm/src/arm_controller.cpp
+++ catkin_workspace/src/kinova_arm/src/arm_controller.cpp
@@ -1,37 +1,8 @@
-// A simple program that computes the square root of a number
-#include <cmath>
-#include <cstdlib>
-#include <iostream>
-#include <string>
-#include <kdl/chain.hpp>
-#include "primitives.h"
-#include "ros/ros.h"
-#include "sensor_msgs/JointState.h"
+#include "arm_controller.h"
 
-void armCallback_0(const sensor_msgs::JointState::ConstPtr& msg)
-{
-    for (int i=0; i<msg->position.size() ; ++i)
-    {
-      ROS_INFO_STREAM("robot 0: " << msg->name[i] << " " << msg->position[i]);
-    }
-}
-
-void armCallback_1(const sensor_msgs::JointState::ConstPtr& msg)
-{
-    for (int i=0; i<msg->position.size() ; ++i)
-    {
-      ROS_INFO_STREAM("robot 1: " << msg->name[i] << " " << msg->position[i]);
-    }
-}
-
-int main(int argc, char **argv)
-{
-
-    ros::init(argc, argv, "pose_listener");
-    ros::NodeHandle n;
-    ros::Subscriber sub_0 = n.subscribe("my_gen3_0/joint_states", 1000, armCallback_0);
-    ros::Subscriber sub_1 = n.subscribe("my_gen3_1/joint_states", 1000, armCallback_1);
-    ros::spin();
-
-    return 0;
+CollisionMonitor::CollisionMonitor(Monitor monitorObject){
+  Eigen::Matrix4d endpose;
+  this->monitor = monitorObject;
+  Arm arm = this->monitor.arm;
+  
 }
