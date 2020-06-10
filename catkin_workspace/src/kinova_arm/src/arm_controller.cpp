dbb1d37028138dc4472742f21fa51b60715fef57
Brennan Penfold <bjp209@hotmail.com>
2020-06-10 11:53:43 +0200

    [ROS] basic subscriber

--- catkin_workspace/src/kinova_arm/src/arm_controller.cpp
+++ catkin_workspace/src/kinova_arm/src/arm_controller.cpp
@@ -5,20 +5,33 @@
 #include <string>
 #include <kdl/chain.hpp>
 #include "primitives.h"
+#include "ros/ros.h"
+#include "sensor_msgs/JointState.h"
 
-int main(int argc, char* argv[])
+void armCallback_0(const sensor_msgs::JointState::ConstPtr& msg)
 {
-  if (argc < 2) {
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
-    return 1;
-  }
+    for (int i=0; i<msg->position.size() ; ++i)
+    {
+      ROS_INFO_STREAM("robot 0: " << msg->name[i] << " " << msg->position[i]);
+    }
+}
 
-  // convert input to double
-  const double inputValue = atof(argv[1]);
+void armCallback_1(const sensor_msgs::JointState::ConstPtr& msg)
+{
+    for (int i=0; i<msg->position.size() ; ++i)
+    {
+      ROS_INFO_STREAM("robot 1: " << msg->name[i] << " " << msg->position[i]);
+    }
+}
 
-  // calculate square root
-  const double outputValue = sqrt(inputValue);
-  std::cout << "The square root of " << inputValue << " is " << outputValue
-            << std::endl;
-  return 0;
+int main(int argc, char **argv)
+{
+
+    ros::init(argc, argv, "pose_listener");
+    ros::NodeHandle n;
+    ros::Subscriber sub_0 = n.subscribe("my_gen3_0/joint_states", 1000, armCallback_0);
+    ros::Subscriber sub_1 = n.subscribe("my_gen3_1/joint_states", 1000, armCallback_1);
+    ros::spin();
+
+    return 0;
 }
