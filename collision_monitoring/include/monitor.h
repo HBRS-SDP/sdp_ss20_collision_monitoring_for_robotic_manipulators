2bcfc38c0b2fda2dfbbc6cd2153ba23e4e570fa5
Samuel Parra <9706510+samuelpg@users.noreply.github.com>
2020-06-10 11:26:30 +0200

    [monitor] added partial documentation

--- collision_monitoring/include/monitor.h
+++ collision_monitoring/include/monitor.h
@@ -6,6 +6,14 @@
 #include "arm.h"
 #include "primitives.h"
 
+/**
+ * A collision monitor with methods to determine the distance to obstacles or links of the arm
+ * 
+ * The Monitor class is a collision monitor which stores the address of an instance of the class Arm
+ * and maintains a list of obstacles. It can perform collision monitoring with obtacles by determining
+ * the distance to obstacles in the workspace, or collision monitoring with the arm itself by 
+ * monitoring the distance between the links.  
+ */
 class Monitor
 {
 
@@ -13,7 +21,10 @@ class Monitor
 
         Arm* arm;
         std::vector<Primitive*> obstacles;
-        // Void on return type of these functions temporally
+
+        /** monitor collision with obstacles. 
+        *
+        */
         std::vector<std::vector<double>> monitorCollisionWithObjects();
         std::vector<std::vector<double>> monitorCollisionWithArm();
         Monitor(Arm* arm, std::vector<Primitive*> obstacles);
