9986ec923a3f74c6744a50694cc748a5c4239153
Alan Gomez <alanorlando95@hotmail.com>
2020-06-26 00:56:38 +0200

    [doxygen] new documentation of 3 methods in primitives

--- deliverables/doxygen/html/search/all_2.js
+++ deliverables/doxygen/html/search/all_2.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['capsule',['Capsule',['../class_capsule.html',1,'Capsule'],['../class_capsule.html#aab6d0827aa8179c3f258129161a67e2e',1,'Capsule::Capsule(Eigen::Matrix4d pose, double length, double radius)'],['../class_capsule.html#a94838c4642111c07dbf83fdc775d1a68',1,'Capsule::Capsule(Capsule *capsule)']]],
   ['controlloop',['controlLoop',['../class_arm_controller.html#ad7e00a70c968a23087415b1bd0ac8bf1',1,'ArmController']]],
-  ['collision_20monitoring',['Collision monitoring',['../md__home_brennan_SDP_sdp_ss20_collision_monitoring_for_robotic_manipulators_collision_monitoring_README.html',1,'']]]
+  ['collision_20monitoring',['Collision monitoring',['../md__home_alan_Documents_MAS_SS20_SDP_sdp_ss20_collision_monitoring_for_robotic_manipulators_collision_monitoring_README.html',1,'']]]
 ];
