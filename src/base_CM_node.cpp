2bfb9a39106a51f0e06c318a9e69c0d456cff5c0
Sreenivasa <hvsreenivasa93@gmail.com>
2021-03-10 13:38:21 +0100

    new updates

--- src/base_CM_node.cpp
+++ src/base_CM_node.cpp
@@ -7,14 +7,15 @@
 #include "primitives.h"
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
-//#include "arm_controller.h"
+// #include "arm_controller.h"
 #include "base_controller.h"
 #include "sensor_msgs/JointState.h"
 #include "visualization_msgs/Marker.h"
+#include "marker_publisher.h"
 
 int main(int argc, char **argv){
 
-    ros::init(argc, argv, "base_controller");
+   ros::init(argc, argv, "base_controller");
     std::string nodeName = ros::this_node::getName();
     #ifdef DEBUG
         std::cout << "nodeName: " << nodeName << std::endl;
@@ -23,7 +24,7 @@ int main(int argc, char **argv){
     std::string goalTopic;
     std::string narkoPosition;
 
-    // setup the first monitor function
+// setup the first monitor function
     n.param<std::string>(ros::this_node::getName()+"/goal_topic", goalTopic, "/goal_point");
     n.param<std::string>(ros::this_node::getName()+"/narko_base_link", narkoPosition, "/narko_base_link");
     double K;
@@ -42,35 +43,40 @@ int main(int argc, char **argv){
 //                   0,0,0,1;
 
 
-    Eigen::Vector3d basePosition;
+  Eigen::Vector3d basePosition;
 
-    basePosition[0]=0.0;
-    basePosition[1]=0.0;
-    basePosition[2]=-1.5;
+  basePosition[0]=0.0;
+  basePosition[1]=0.0;
+  basePosition[2]=-1.5;
+ 
     NarkinBase base1(basePosition);
     // KinovaArm arm1(model);
     Monitor monitor1(&base1);
-    double radius_1 = 30;
-    Eigen::Matrix4d pose_1;
-    pose_1 << 0.12071974, -0.60161281, -0.78961305, 25,
-                -0.60161281,  0.58837018, -0.54026156, 70,
-                0.78961305,  0.54026156, -0.29091007, 20,
-                0,          0,          0,          1;
-    Sphere *sphere_1 = new Sphere(pose_1, radius_1);
-    monitor1.addObstacle(sphere_1);
+    // double radius_1 = 30;
+    // Eigen::Matrix4d pose_1;
+    // pose_1 << 0.0, 0.0, 0.0,3,
+    //           0.0, 0.0, 0.0,3,
+    //           0.0, 0.0, 0.0,0,
+    //           0  , 0  , 0  ,1;
+
+    // Sphere *sphere_1 = new Sphere(pose_1, radius_1);
+            
+    // monitor1.addObstacle(sphere_1);
+
     Eigen::Vector3d initPose;
     initPose[0]=0.0; // x position of center of box
     initPose[1]=0.0; // y position of center of box
     initPose[2]=1.5; // z position of center of box
     base1.updatePose(initPose);
 
-    // Create the armController class based off the first monitor
+    // Create the baseController class based off the first monitor
 
     BaseController baseController1(&monitor1, K, D, gamma, beta);
     
     // Init ROS listener
     ros::Subscriber goalSub = n.subscribe(goalTopic, 1000, &BaseController::goalCallback, &baseController1);
     ros::Subscriber obstacleSub = n.subscribe("kinova_controller/obstacles", 1000, &BaseController::updateObstacles, &baseController1);
+    ros::Publisher obstaclePub_sim = n.advertise<visualization_msgs::Marker>("kinova_controller/obstacles", 1000);
     ros::Subscriber basePosSub = n.subscribe("/odom", 1000, &BaseController::baseCallback, &baseController1);
 
     ros::Publisher markersPub = n.advertise<visualization_msgs::Marker>("kinova_controller/markers", 1000);
@@ -83,9 +89,15 @@ int main(int argc, char **argv){
     geometry_msgs::Twist baseVelocity;
     
     while(ros::ok()) {
-        baseVelocity = baseController1.control_loop();
 
+    //    MarkerPublisher mPublisher(obstaclePub_sim, visualization_msgs::Marker::SPHERE, "base_link", "obstacles", 0, 3, 3, 0, 255, 2, 0, 0.5);
+    //    mPublisher.setRadius(0.3);
 
+    //    mPublisher.Publish();
+
+       baseVelocity = baseController1.control_loop();
+       
+       
         baseVelPub.publish(baseVelocity);
         for(int i=0; i<baseController1.rvizObstacles.size(); i++){
             markersPub.publish(baseController1.rvizObstacles[i]->marker);
@@ -102,3 +114,74 @@ int main(int argc, char **argv){
 
 
 
+
+//Eigen::Vector3d  Box3::OwnClosestPoint(Sphere *sphere)
+// {    
+//      Eigen::Vector3d l_pt1[8];
+//      Eigen::Vector3d l_pt2[6];
+//      Eigen::Vector3d sideCenters[6];
+//      Eigen::Vector3d Corners[8];
+//     double  distance;
+//     double min_distance;
+//     Eigen::Vector3d closest_point;
+
+
+//       for (int i = 0; i < 8; i++)
+//     {
+        
+//            Corners[i]=this->CornerPoint(i); 
+             
+//         // cout<< "Corner point " <<i<<" = "<<endl<<Corners[i]<<endl;
+          
+//     }
+
+
+   
+//       for (int i = 0; i < 6; i++)
+//     {
+        
+//            sideCenters[i]=this->SideCenterPoint(i); 
+             
+//         // cout<< "Corner point " <<i<<" = "<<endl<<Corners[i]<<endl;
+          
+//     }
+     
+
+// for (int i = 0; i < 8; i++)
+// {     
+//       Eigen::Vector3d c_pt = Corners[i];
+//       l_pt1[i] = sphere->getClosestPointToPoint(c_pt);
+           
+      
+//       distance = std::sqrt((std::pow(l_pt1[i][0]-c_pt[0],2)+std::pow(l_pt1[i][1]-c_pt[1],2)+std::pow(l_pt1[i][2]-c_pt[2],2))*1.0);
+//       if (distance<min_distance){
+//           min_distance=distance;
+//           closest_point= c_pt;
+//       }
+
+
+// }
+
+
+
+// for (size_t i = 0; i < 6; i++)
+// {
+    
+//       Eigen::Vector3d c_pt = sideCenters[i];
+//       l_pt2[i] = sphere->getClosestPointToPoint(c_pt);
+           
+      
+//       distance= std::sqrt((std::pow(l_pt2[i][0]-c_pt[0],2)+std::pow(l_pt2[i][1]-c_pt[1],2)+std::pow(l_pt2[i][2]-c_pt[2],2))*1.0);
+//       if (distance<min_distance){
+//           min_distance=distance;
+//           closest_point= c_pt;
+//       }
+// }
+
+
+
+// // std::cout<<"minmum distance to line from box : "<<min_distance<< std::endl;
+ 
+//  return closest_point;
+
+// }
--- src/base_controller.cpp
+++ src/base_controller.cpp
@@ -16,16 +16,22 @@
     this->D = d;
     this->gamma = gamma;
     this->beta = beta;
+
     // PID constants should be declared here
     //P= ;
     //I= ;
     //D= ;
-    speed.linear.x=0.0;
-    speed.linear.y=0.0;
-    speed.linear.z=0.0;
-    speed.angular.x=0.0;
-    speed.angular.y=0.0;
-    speed.angular.z=0.0;
+
+
+  speed.linear.x=0.0;
+  speed.linear.y=0.0;
+  speed.linear.z=0.0;
+  speed.angular.x=0.0;
+  speed.angular.y=0.0;
+  speed.angular.z=0.0;
+
+
+
      
     // ======== need to describe these topics to be subscribed these are for potnetial fields
 
@@ -84,12 +90,14 @@ void BaseController::goalCallback(const geometry_msgs::Point::ConstPtr& msg){
     this->goal[1] = msg->y;
     this->goal[2] = msg->z;
 
+   
+
 }
 
 
 void BaseController::updateObstacles(const visualization_msgs::Marker::ConstPtr& msg){
 
-    std::cout << "New obstacle of type: " << msg->type <<std::endl;
+std::cout << "New obstacle of type: " << msg->type <<std::endl;
     if (msg->type == visualization_msgs::Marker::SPHERE) {
         bool newObstacle = true;
         for(int i=0; i<rvizObstacles.size();i++){
@@ -132,9 +140,9 @@ void BaseController::updateObstacles(const visualization_msgs::Marker::ConstPtr&
             monitor->addObstacle(capsule);
         }
     }
-    // ==================== Box obstacle ======================================
+// ==================== Box obstacle ======================================
 
-    else if(msg->type == visualization_msgs::Marker::CUBE){
+ else if(msg->type == visualization_msgs::Marker::CUBE){
         bool newObstacle = true;
         for(int i=0; i<rvizObstacles.size();i++){
             if(rvizObstacles[i]->marker.id == msg->id) {
@@ -155,7 +163,7 @@ void BaseController::updateObstacles(const visualization_msgs::Marker::ConstPtr&
 
 
     
-    //========================================================================
+//========================================================================
 
     else {
        // ROS_ERROR("Wrong shape for obstacle");
@@ -168,24 +176,30 @@ void BaseController::updateObstacles(const visualization_msgs::Marker::ConstPtr&
 
 Eigen::Vector3d BaseController::obstaclePotentialField(Eigen::Vector3d currentPosition, 
                                         Eigen::Vector3d velocity) {
-    // init the variables used in the maths
+       // init the variables used in the maths
     Eigen::Vector3d potentialField = Eigen::Vector3d({0, 0, 0});
     Eigen::Vector4d origin(0, 0, 0, 1);
     Eigen::Vector3d startArrow, positionLink;
     Eigen::Vector3d ownClosestPoint, obstacleClosestPoint;
-    // ROS_ERROR_STREAM(" field function called: \n ");  
-    Box3 *baseCube;
-    double angle = 3.1415/2;
+     //ROS_ERROR_STREAM(" field function called: \n ");  
+     Box3 *baseCube;
+     double angle = 3.1415/2;
      
-    std::vector<Primitive*> obstacles;
-    obstacles.reserve(monitor->obstacles.size());
-    obstacles.insert(obstacles.end(), monitor->obstacles.begin(), monitor->obstacles.end());
-    //   obstacles.pop_back();
 
-    for(int i = 0; i < obstacles.size(); i++) {
-        Eigen::Vector3d direction; 
-        // ROS_ERROR_STREAM(" field function called in loop: \n "<< typeid(obstacles[i]).name()); 
-        // ROS_ERROR_STREAM(" field function called before shortest distance : \n "); 
+     if (monitor->obstacles.empty()!=0){
+      std::vector<Primitive*> obstacles; 
+      obstacles.reserve(monitor->obstacles.size());
+      obstacles.insert(obstacles.end(), monitor->obstacles.begin(), monitor->obstacles.end());
+    //   obstacles.pop_back();
+     
+      for(int i = 0; i < obstacles.size(); i++) {
+        Eigen::Vector3d direction;
+         //ROS_ERROR_STREAM(" field function called in loop: \n "<< typeid(obstacles[i]).name()); 
+
+   
+              
+         // ROS_ERROR_STREAM(" field function called before shortest distance : \n "); 
+        
         monitor->base->base_primitive->getShortestDirection(direction, 
                                             obstacles[i]);
 
@@ -202,23 +216,37 @@ Eigen::Vector3d BaseController::obstaclePotentialField(Eigen::Vector3d currentPo
              
             ownClosestPoint = closestPoints.row(0);
             obstacleClosestPoint = closestPoints.row(1);
+            Eigen::Vector3d h={1,1,0};
+             obstacleClosestPoint=h;
+
+
 
             startArrow = baseCube->box_center; // currently arrow starting from center of cube , needs to change to own closest point 
-            //=== need to chamge it to narko base topic   
-            MarkerPublisher mPublisherShortestDistance(arrowsPub_base, visualization_msgs::Marker::ARROW, "Narko_base_link_bluearrow", "shortest_distance_base_arrow", i, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0); 
+         //=== need to chamge it to narko base topic   
+            MarkerPublisher mPublisherShortestDistance(arrowsPub_base, visualization_msgs::Marker::ARROW, "Narko_base_link", "shortest_distance_base_arrow", i, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0); 
             mPublisherShortestDistance.setRadius(0.005);
             mPublisherShortestDistance.setLength(0.005);
             mPublisherShortestDistance.setPoints(obstacleClosestPoint, ownClosestPoint);// + (direction / direction.norm()) * endEffectorCapsule->getRadius() );
             mPublisherShortestDistance.Publish();
             }
 
-        //   ROS_ERROR_STREAM(" basecontroller reached here: \n ");   
+         // ROS_ERROR_STREAM(" basecontroller reached here: \n ");   
+        
+        // Eigen::Vector3d retFiled = {0.1,0.1,0.1};
+
+        //  return retFiled; 
+
         if (velocity.norm() < 1.0e-2) {
             return potentialField;
-            // ROS_WARN_STREAM("PField in basecontroller inside if: \n " << potentialField<<"\n\n");
+           // ROS_WARN_STREAM("PField in basecontroller inside if: \n " << potentialField<<"\n\n");
         } 
+        
+ 
+        // Rotation matrix
         Eigen::Vector3d rotvec = direction.cross(velocity);
         rotvec.normalized();
+
+        // Build the rotation matrix from the rodreges vector
         double ct = cos(angle);
         double st = sin(angle);
         double vt = 1-ct;
@@ -231,6 +259,7 @@ Eigen::Vector3d BaseController::obstaclePotentialField(Eigen::Vector3d currentPo
         double m_vt_0_1=m_vt_0*rotvec(1);
         double m_vt_0_2=m_vt_0*rotvec(2);
         double m_vt_1_2=m_vt_1*rotvec(2);
+
         Eigen::Matrix3d rotation;
         rotation << ct + m_vt_0*rotvec(0), 
                     -m_st_2 +  m_vt_0_1,
@@ -241,35 +270,62 @@ Eigen::Vector3d BaseController::obstaclePotentialField(Eigen::Vector3d currentPo
                     -m_st_1 +  m_vt_0_2,
                     m_st_0  +  m_vt_1_2,
                     ct      +  m_vt_2*rotvec(2);
+
+        //Perform the related maths from [1]
         rotation.normalize();
+       
+       
+       
+        // Phi
+        // φ = cos ((o − x) v/(|o − x| · |v|))
         // double phi_denominator = velocity.norm()*direction.norm();
+
         Eigen::Vector3d velocity_abs = velocity.cwiseAbs();
         Eigen::Vector3d direction_abs = direction.cwiseAbs();
+
         double phi_denominator= velocity_abs.dot(direction_abs); 
-        double phi_numerator = direction.transpose()*velocity;
+        // double phi_denominator =direction_abs.adjoint()* velocity_abs;
+
+         
+       double phi_numerator = direction.dot(velocity);
+        // double phi_numerator = direction.transpose()*velocity;
         double phi = std::acos(phi_numerator/phi_denominator);
+
         double exp = std::exp(-beta*phi);
+
+        // R i vφ i exp(−βφ i )
         potentialField += (rotation * velocity) * phi * exp;
        // ROS_WARN_STREAM("PField in basecontroller: \n " << potentialField<<"\n\n");
+   
         MarkerPublisher mPublisherPotentialField(arrowsPub_base, visualization_msgs::Marker::ARROW, "base_link_arrow", "potential_field_base", i, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0);
+        
         mPublisherPotentialField.setRadius(0.005);
         mPublisherPotentialField.setLength(0.005);
         mPublisherPotentialField.setPoints(obstacleClosestPoint, obstacleClosestPoint + (gamma * potentialField));
         mPublisherPotentialField.Publish();
+    
+        
         }
+
         return gamma * potentialField;
+
+     }
+
+     else {   Eigen::Vector3d zeroField ={0,0,0};
+            
+         return  zeroField;}
     }
 
 geometry_msgs::Twist BaseController:: control_loop(){
     // Variable for storing the resulting joint velocities
     double x, y, z;
-    // Update the current state to match real arm state
+    // Update the current state to match real base state
     this->monitor->base->updatePose(this->basePosition);
 
     // Eigen::Matrix4d currBasePose = monitor->base->getPose();
     Eigen::Vector3d currBasePoint =  monitor->base->getPose();;
     objectDistances = monitor->baseDistanceToObjects();
-
+    // ROS_WARN_STREAM(objectDistances[0]);
     // Show base as box
 
     Box3 *cubebase;
@@ -291,42 +347,69 @@ geometry_msgs::Twist BaseController:: control_loop(){
         // scale[2]=cubebase->extents[2]/2;
         
             MarkerPublisher mPublisherLink(CubePub, visualization_msgs::Marker::CUBE, "narko_base_link_marker_cpp", "base _link_cpp", 20, positionLink(0), positionLink(1), positionLink(2), 0.0, 0.0, 1.0, 0.5);
-        mPublisherLink.setScale(scale);                       
-        mPublisherLink.Publish();
+            mPublisherLink.setScale(scale);                       
+            mPublisherLink.Publish();
 
     }
   
-    geometry_msgs::Twist speed;
-    double inc_x= goal[0] - basePosition[0];
-    double  inc_y= goal[1] - basePosition[1];
-    auto th_to_follow = atan2( inc_y,inc_x ) ; 
-    double  inc_th= th_to_follow-basePosition[2];
+  geometry_msgs::Twist speed;
+
+double inc_x= goal[0] - basePosition[0];
+double  inc_y= goal[1] - basePosition[1] ;
+auto th_to_follow = atan2( inc_y,inc_x ) ; 
+double  inc_th= th_to_follow-basePosition[2];
 
 
-    //   speed.linear.x=inc_x*0.1;
-    //   speed.linear.y=inc_y*0.1;
-    //   speed.angular.z=inc_th*0.1;
-    Eigen::Vector3d destination = {inc_x, inc_y, inc_th};
-    Eigen::Vector3d my_field = {0.1, 0.2, 0.4};
+//   speed.linear.x=inc_x*0.1;
+//   speed.linear.y=inc_y*0.1;
+//   speed.angular.z=inc_th*0.1;
+
+     
+
+
+
+
+
+Eigen::Vector3d destination = {inc_x, inc_y, inc_th};
+
+Eigen::Vector3d my_field = {0.1, 0.2, 0.4};
+
+
+            // calculating difference between current and goal orientation of robot base  
+            //    auto th_to_follow = atan2( inc_y,inc_x ) ;          
+
+       
                 
-    Eigen::Vector3d currVelocity = {speed.linear.x, speed.linear.y, speed.angular.z};
-    Eigen::Vector3d newVelocity = K * (destination) - D * currVelocity
+  Eigen::Vector3d currVelocity = {speed.linear.x, speed.linear.y, speed.angular.z};
+   Eigen::Vector3d newVelocity = K * (destination) - D * currVelocity
                                 + BaseController::obstaclePotentialField(currBasePoint, 
                                 currVelocity);
 
 
+
+// Eigen::Vector3d currVelocity = {speed.linear.x, speed.linear.y, speed.angular.z};
+//    Eigen::Vector3d newVelocity = K * (destination) - D * currVelocity
+//                                 + my_field ;
+
+
      
     Eigen::Vector4d transformedVel = {newVelocity[0], newVelocity[1], newVelocity[2], 0.0};
+    // transformedVel = monitor->arm->getPose() * transformedVel;
     x = newVelocity[0];
     y = newVelocity[1];
     z = newVelocity[2];
+
     speed.linear.x = x;
     speed.linear.y = y;
     speed.angular.z = z; 
+
     return speed; 
 
 
 }
+
+
+
 RvizObstacle::RvizObstacle(visualization_msgs::Marker::ConstPtr markerIn, int index) {
     marker = *markerIn;
     idx = index;
@@ -342,8 +425,10 @@ RvizObstacle::RvizObstacle(visualization_msgs::Marker::ConstPtr markerIn, int in
             2*qx*qz-2*qy*qw, 2*qy*qz+2*qx*qw, 1-2*qx*qx-2*qy*qy, pz,
             0, 0, 0, 1;
 }
+
 RvizObstacle::~RvizObstacle() {
 }
+
 Eigen::Matrix4d RvizObstacle::updatePose(visualization_msgs::Marker::ConstPtr markerIn) {
     marker = *markerIn;
     double qx = marker.pose.orientation.x;
@@ -359,3 +444,7 @@ Eigen::Matrix4d RvizObstacle::updatePose(visualization_msgs::Marker::ConstPtr ma
             0, 0, 0, 1;
     return pose;
 }
+
+
+
+
